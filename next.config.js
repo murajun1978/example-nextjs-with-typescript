a316424c78ec6f17bf7285a57ee0b716fd84da03
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2019-07-02 13:56:28 +0900

    Add linter

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config();
 
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const withTypescript = require('@zeit/next-typescript');
+/* eslint-enable */
 
 module.exports = withTypescript({
   webpack: config => {
@@ -12,10 +14,10 @@ module.exports = withTypescript({
       ...config.plugins,
       new Dotenv({
         path: path.join(__dirname, '.env'),
-        systemvars: true
-      })
+        systemvars: true,
+      }),
     ];
 
     return config;
-  }
+  },
 });
